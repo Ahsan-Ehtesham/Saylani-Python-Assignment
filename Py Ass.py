d51bd996e024fe5eccf802ca4f301a36b0dc2a84
Ahsan Ehtesham <ahsanehtesham110@gmail.com>
2019-11-03 12:58:14 +0500

    Python Assignment 1

---
+++ Py Ass.py
@@ -0,0 +1,28 @@
+'''print("Twinkle, twinkle, little star,")
+print('\t',"How I wonder what you are!")
+print('\t','\t',"Up above the world so high,")
+print('\t','\t',"Like a diamond in the sky.")
+print("Twinkle, twinkle, little star,")
+print('\t',"How I wonder what you are")'''
+
+'''import sys
+print("Python version")
+print (sys.version)'''
+
+'''from datetime import datetime
+now = datetime. now()
+print(now)'''
+
+'''radius=int(input("Enter Radius:"))
+area=3.142*(radius**2)
+print("Area of circle:",area)'''
+
+'''First=input("Enter First Name:")
+Last=input("Enter Last Name:")
+name=Last[::-1]+ " " +First[::-1]
+print(name)'''
+
+a=int(input("Enter a:"))
+b=int(input("Enter b:"))
+x=a+b
+print(x)
